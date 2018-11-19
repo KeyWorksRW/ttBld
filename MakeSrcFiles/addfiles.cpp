4a28dd8ee8cbd42545f1241875365ddf767b46f7
KeyWorksRW <42786259+KeyWorksRW@users.noreply.github.com>
2018-11-19 06:06:57 -0800

    Initial code

--- MakeSrcFiles/addfiles.cpp
+++ MakeSrcFiles/addfiles.cpp
@@ -9,7 +9,99 @@
 #include "precomp.h"
 
 #include "../ttLib/include/strlist.h"	// CStrList
+#include "../ttLib/include/keyfile.h"	// CKeyFile
+
+#include "../common/csrcfiles.h"		// CSrcFiles
 
 void AddFiles(CStrList& lstFiles)
 {
+	if (lstFiles.GetCount() < 1) {
+		puts("You didn't specify any files to add!");
+		return;
+	}
+
+	CSrcFiles cSrcFiles;
+	cSrcFiles.ReadFile(".srcfiles");
+
+	for (size_t pos = 0; pos < lstFiles.GetCount(); ++pos) {
+		if (cSrcFiles.m_lstSrcFiles.Find(lstFiles[pos])) {
+			printf("%s already in .srcfiles\n", lstFiles[pos]);
+			cSrcFiles.m_lstSrcFiles.Remove(pos);
+		}
+	}
+
+	size_t cFilesAdded = 0;
+
+	CKeyFile kfIn, kfOut;
+	if (!kfIn.ReadFile(".srcfiles")) {
+		puts("Cannot read .srcfiles!");
+		return;
+	}
+
+	while (kfIn.readline() && !isSameSubString(kfIn, "Files:"))
+		kfOut.WriteEol(kfIn);
+
+	if (kfOut.IsEndOfFile()) {	// means there was no Files: section
+		kfOut.WriteEol("Files:");
+		for (size_t pos = 0; pos < lstFiles.GetCount(); ++pos) {
+			if (!FileExists(lstFiles[pos]))	{
+				printf("%s doesn't exist -- not added\n", lstFiles[pos]);
+				continue;
+			}
+			kfOut.printf("  %s\n", lstFiles[pos]);
+			++cFilesAdded;
+		}
+		if (!kfOut.WriteFile(".srcfiles")) {
+			puts("Cannot write to .srcfiles!");
+			return;
+		}
+
+		// Use CStr to print the count to properly add the "s" to "file" as needed. I.e., 0 files, 1 file, 2 files -- 1
+		// file is singular
+
+		CStr cszResults;
+		cszResults.printf("%u file%ks added.", cFilesAdded, cFilesAdded);
+		puts(cszResults);
+		return;
+	}
+
+	kfOut.WriteEol(kfIn);	// This will be the Files: line
+	while (kfIn.readline()) {
+		if (IsAlpha(*(const char*)kfIn))	// Alphabetical character in first column is a new section
+			break;
+		else if (!FindNonSpace(kfIn)) {		// blank line, insert after it
+			kfOut.WriteEol(kfIn);
+			break;
+		}
+		else
+			kfOut.WriteEol(kfIn);
+	}
+
+	// We are now after a blank line, at the beginning of a section, or at the end of the file
+
+	for (size_t pos = 0; pos < lstFiles.GetCount(); ++pos) {
+		if (!FileExists(lstFiles[pos]))	{
+			printf("%s doesn't exist -- not added\n", lstFiles[pos]);
+			continue;
+		}
+		kfOut.printf("  %s\n", lstFiles[pos]);
+		++cFilesAdded;
+	}
+
+	// Now that the file(s) have been inserted, add everything else
+
+	while (kfIn.readline())
+		kfOut.WriteEol(kfIn);
+
+	if (!kfOut.WriteFile(".srcfiles")) {
+		puts("Cannot write to .srcfiles!");
+		return;
+	}
+
+	// Use CStr to print the count to properly add the "s" to "file" as needed. I.e., 0 files, 1 file, 2 files -- 1
+	// file is singular
+
+	CStr cszResults;
+	cszResults.printf("%u file%ks added.", cFilesAdded, cFilesAdded);
+	puts(cszResults);
 }
