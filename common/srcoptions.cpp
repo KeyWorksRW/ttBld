418ae83d9a1386e07416a339cb7b8078ce378ad5
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2019-03-07 14:25:00 -0800

    Reworks the way boolean options get handled

--- common/srcoptions.cpp
+++ common/srcoptions.cpp
@@ -17,12 +17,14 @@ CSrcOption::CSrcOption()
 	m_pszName    = nullptr;
 	m_pszVal     = nullptr;
 	m_pszComment = nullptr;
+	m_bBoolean = false;
 	m_bRequired = false;
 }
 
-void CSrcOption::AddOption(const char* pszName, bool bRequired)
+void CSrcOption::AddOption(const char* pszName, bool bBoolean, bool bRequired)
 {
 	m_pszName = pszName;
+	m_bBoolean = bBoolean;
 	m_bRequired = bRequired;
 }
 
@@ -30,26 +32,8 @@ void CSrcOption::AddOption(const char* pszName, bool bRequired)
 
 void CSrcOption::UpdateOption(bool bValue, const char* pszComment)
 {
-	if (bValue) {
-		if (m_pszVal) {
-			if (m_pszVal[0] != 't') {
-				tt::FreeAlloc(m_pszVal);
-				m_pszVal = tt::StrDup("true");
-			}
-		}
-		else
-			m_pszVal = tt::StrDup("true");
-	}
-	else {
-		if (m_pszVal) {
-			if (m_pszVal[0] != 'f') {
-				tt::FreeAlloc(m_pszVal);
-				m_pszVal = tt::StrDup("false");
-			}
-		}
-		else
-			m_pszVal = tt::StrDup("false");
-	}
+	m_bBoolean = true;
+	m_bVal = bValue;
 
 	if (!pszComment) {
 		if (m_pszComment) {
@@ -63,27 +47,35 @@ void CSrcOption::UpdateOption(bool bValue, const char* pszComment)
 	m_pszComment = tt::StrDup(pszComment);
 }
 
-// It's important that if a zero-length string is passed in, then the matching value or comment should be set to a
-// nullptr. This makes it so that callers only need to check for a nullptr to determine if there is a valid value or
-// comment.
+// It's important that if a zero-length string is passed in for either parameter, then the matching value or comment
+// should be set to a nullptr. This makes it so that callers only need to check for a nullptr to determine if there is a
+// valid value or comment.
 
 void CSrcOption::UpdateOption(const char* pszVal, const char* pszComment)
 {
-	if (!pszVal || !*pszVal) {
-		if (m_pszVal) {
-			tt::FreeAlloc(m_pszVal);
-			m_pszVal = nullptr;
-		}
+	if (m_bBoolean) {
+		if (pszVal)
+			m_bVal = tt::isSameStri(pszVal, "true") || tt::isSameStri(pszVal, "yes");
+		else
+			m_bVal = false;
 	}
 	else {
-		if (m_pszVal) {
-			if (!tt::isSameStr(m_pszVal, pszVal)) {		// don't allocate if nothing will change
+		if (!pszVal || !*pszVal) {
+			if (m_pszVal) {
 				tt::FreeAlloc(m_pszVal);
-				m_pszVal = tt::StrDup(pszVal);
+				m_pszVal = nullptr;
 			}
 		}
-		else
-			m_pszVal = tt::StrDup(pszVal);
+		else {
+			if (m_pszVal) {
+				if (!tt::isSameStr(m_pszVal, pszVal)) {		// don't allocate if nothing will change
+					tt::FreeAlloc(m_pszVal);
+					m_pszVal = tt::StrDup(pszVal);
+				}
+			}
+			else
+				m_pszVal = tt::StrDup(pszVal);
+		}
 	}
 
 	if (!pszComment || !*pszComment) {
@@ -102,4 +94,12 @@ void CSrcOption::UpdateOption(const char* pszVal, const char* pszComment)
 		else
 			m_pszComment = tt::StrDup(pszComment);
 	}
+}
+
+const char* CSrcOption::getOption()
+{
+	if (!m_bBoolean)
+		return m_pszVal;
+	else
+		return (m_bVal ? "true" : nullptr);
 }
--- common/srcoptions.h
+++ common/srcoptions.h
@@ -28,23 +28,21 @@ public:
 	const char* getName() { return m_pszName; }
 	const char* getComment() { return m_pszComment; }
 
-	// The following two functions assume you already know if the option uses a string value or a boolean value
+	const char* getOption();	// for boolean, will return pointer to "true" for true, or nullptr for false
+	bool 		getBoolOption()   { ttASSERT(m_bBoolean); return m_bVal; }	// in case you really, really need a bool return
 
-	const char* getStringOption() { return m_pszVal; }
-	bool 		getBoolOption()   { return (m_pszVal ? m_pszVal[0] == 't' : false); }
-
-	void AddOption(const char* pszName, bool bRequired = false);
+	void AddOption(const char* pszName, bool bBoolean = false, bool bRequired = false);
 	void UpdateOption(bool bValue, const char* pszComment = nullptr);
-	void UpdateOption(const char* pszVal, const char* pszComment = nullptr);
+	void UpdateOption(const char* pszVal, const char* pszComment = nullptr);	// fine to call this for boolean options if pszVal == "true/false" or "yes/no"
 
 protected:
 	// Class members
 
-	// Note: for boolean options, m_pszVal will be 0 or 1
-
 	const char* m_pszName;	// points to callers string, NOT allocated
-	char*		m_pszVal;
+	char*		m_pszVal;	// only used if m_bBoolean is false
 	char*		m_pszComment;
+	bool		m_bVal;		// only used if m_bBoolean is true
 
+	bool	m_bBoolean;		// argument is a boolean value
 	bool	m_bRequired;
 };
