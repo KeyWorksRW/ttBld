5808be3a6f25326f3edfc777a08b9bf5ceb168ce
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2019-05-25 08:31:40 -0700

    Removes private dialog and ids

--- src/resource.h
+++ src/resource.h
@@ -2,7 +2,7 @@
 // Microsoft Developer Studio generated include file.
 // Used by makeninja.rc
 //
-#define IDDLG_SRCFILES                  100
+
 #define IDDDLG_CONVERT                  101
 #define IDDLG_OPTIONS                   102
 #define IDTAB_GENERAL                   103
@@ -11,7 +11,6 @@
 #define IDTAB_SCRIPTS                   106
 #define IDTAB_RCMIDL                    107
 #define IDTAB_CLANG                     108
-#define IDTAB_PRIVATE                   109
 #define IDR_PRE_PROJECT                 1024
 #define IDEDIT_PROJ_NAME                1024
 #define IDR_POST_PROJECT                1025
@@ -23,7 +22,6 @@
 #define IDRADIO_DLL                     1028
 #define IDCHECK_CODEBLOCKS              1029
 #define IDR_VCXPROJ_MASTER              1030
-#define IDCHECK_VCXPROJ                 1030
 #define IDR_VCXPROJ_FILTERS             1031
 #define IDCHECK_CODELITE                1031
 #define IDCHECK_CLANG                   1032
@@ -31,7 +29,7 @@
 #define IDEDIT_LIBS                     1034
 #define IDEDIT_LIBS_BUILD               1034
 #define IDCHECK_64BIT                   1035
-#define IDCHECK_BITEXT                  1036
+#define IDBTN_ADD_LIBDIR                1036
 #define IDCHECK_STATIC_CRT              1037
 #define IDEDIT_PCH                      1038
 #define IDBTN_PCH                       1039
@@ -39,18 +37,15 @@
 #define IDRADIO_WARN3                   1041
 #define IDRADIO_WARN2                   1042
 #define IDRADIO_WARN1                   1043
-#define IDEDIT_CFLAGS                   1044
 #define IDEDIT_PCH_CPP                  1044
-#define IDEDIT_LINK_FLAGS               1045
 #define IDBTN_PCH_CPP                   1045
-#define IDLIST_FILES                    1046
+#define IDBTN_ADD_LIB                   1046
 #define IDBTN_ADD                       1047
-#define IDBTN_REMOVE                    1048
+#define IDEDIT_CFLAGS                   1048
 #define IDRADIO_MF_NEVER                1049
 #define IDRADIO_MF_MISSING              1050
 #define IDRADIO_MF_ALWAYS               1051
-#define IDRADIO_MF_CLANG                1052
-#define IDRADIO_MF_MSVC                 1053
+
 #define IDC_RADIO_SPACE                 1055
 #define IDC_RADIO_SPEED                 1056
 #define IDC_CHECK_PERMISSIVE            1057
@@ -61,14 +56,12 @@
 #define IDEDIT_LIBDIRS                  1062
 #define IDCHECK_MSRC                    1062
 #define IDEDIT_LIBS_LINK                1063
-#define IDC_BTN_HELP                    1064
-#define IDRADIO_MF_DEBUG                1065
+
 #define IDEDIT_OUT_DIR                  1065
-#define IDRADIO_MF_RELEASE              1066
 #define IDBTN_CHANGE_OUT                1066
 #define IDEDIT_IN_DIR                   1067
 #define IDBTN_CHANGE_IN                 1068
-#define IDCHECK_SCRIPT                  1069
+
 #define IDCOMBO_SCRIPTS                 1070
 #define IDBTN_LOCATE_SCRIPT             1071
 #define IDTXT_FILES_FOUND               1072
@@ -81,7 +74,6 @@
 #define IDEDIT_COMMON                   1079
 #define IDEDIT_DIR64                    1080
 #define IDEDIT_RELEASE                  1080
-#define IDEDIT_COMMON_RC                1080
 #define IDBTN_DIR64                     1081
 #define IDEDIT_DEBUG                    1081
 #define IDCHECK_NO_MSVC                 1082
@@ -90,23 +82,14 @@
 #define IDEDIT_NATVIS                   1086
 #define IDEDIT_DEBUG_MIDL               1087
 #define IDBTN_CHANGE                    1087
-#define IDBTN_BASE_INCDIRS              1088
-#define IDBTN_ADD_LIBDIR                1088
-#define IDBTN_BASE_CFLAGS               1089
-#define IDBTN_ADD_LIB                   1089
-#define IDBTN_BASE_RCFLAGS              1090
-#define IDBTN_BASE_LIBS                 1091
-#define IDCHECK_NO_GS_DBG               1092
-#define IDCHECK_NO_GS_rel               1093
-#define IDCHECK_GS_ROOT                 1094
 
 // Next default values for new objects
-// 
+//
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        109
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1095
+#define _APS_NEXT_CONTROL_VALUE         1088
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
