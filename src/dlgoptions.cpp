7cb981ffa439d2a29a14be6d2a1f3ed28ee6e065
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2019-05-26 09:45:36 -0700

    Detabify files
    These got missed in the last detabify pass

--- src/dlgoptions.cpp
+++ src/dlgoptions.cpp
@@ -1,9 +1,9 @@
 /////////////////////////////////////////////////////////////////////////////
-// Name:		CTabOptions
-// Purpose:		IDDLG_OPTIONS dialog handler
-// Author:		Ralph Walden
-// Copyright:	Copyright (c) 2019 KeyWorks Software (Ralph Walden)
-// License:		Apache License (see ../LICENSE)
+// Name:      CTabOptions
+// Purpose:   IDDLG_OPTIONS dialog handler
+// Author:    Ralph Walden
+// Copyright: Copyright (c) 2019 KeyWorks Software (Ralph Walden)
+// License:   Apache License (see ../LICENSE)
 /////////////////////////////////////////////////////////////////////////////
 
 // As a way to ensure that .private/.srcfiles projects works correctly, we conditionalize a private tab to add to the end
@@ -19,191 +19,197 @@
 
 bool ChangeOptions(bool bDryRun)
 {
-	CTabOptions dlg;
-	if (bDryRun)
-		dlg.EnableDryRun();
+    CTabOptions dlg;
+    if (bDryRun)
+        dlg.EnableDryRun();
 
-	HWND hwndDlg = dlg.DoModeless(NULL);
+    HWND hwndDlg = dlg.DoModeless(NULL);
 
-	// We aren't a window app, so we need to run a message loop here to handle the modeless dialog box
+    // We aren't a window app, so we need to run a message loop here to handle the modeless dialog box
 
-	MSG msg;
-	while (GetMessage(&msg, NULL, 0, 0)) {
-		if (!IsDialogMessage(hwndDlg, &msg)) {
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-		}
-	}
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        if (!IsDialogMessage(hwndDlg, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
 
-	auto result = (msg.message == WM_QUIT) ? msg.wParam : IDCANCEL;
+    auto result = (msg.message == WM_QUIT) ? msg.wParam : IDCANCEL;
 
-	if (result == IDOK) {
-		dlg.SaveChanges();
-		return true;
-	}
-	else
-		return false;
+    if (result == IDOK)
+    {
+        dlg.SaveChanges();
+        return true;
+    }
+    else
+        return false;
 }
 
 CTabOptions::CTabOptions() : ttCDlg(IDDLG_OPTIONS)
 {
-	m_tabGeneral.SetParentClass(this);
-	m_tabCompiler.SetParentClass(this);
-	m_tabCLang.SetParentClass(this);
-	m_tabLinker.SetParentClass(this);
-	m_tabRcMidl.SetParentClass(this);
-	m_tabScripts.SetParentClass(this);
+    m_tabGeneral.SetParentClass(this);
+    m_tabCompiler.SetParentClass(this);
+    m_tabCLang.SetParentClass(this);
+    m_tabLinker.SetParentClass(this);
+    m_tabRcMidl.SetParentClass(this);
+    m_tabScripts.SetParentClass(this);
 #ifdef PRIVATE
-	m_tabPrivate.SetParentClass(this);
+    m_tabPrivate.SetParentClass(this);
 #endif
 
-	ReadFile();	// read in any existing .srcfiles
+    ReadFile(); // read in any existing .srcfiles
 
-	if (ttIsEmpty(GetPchHeader())) {
-		if (ttFileExists("stdafx.h"))
-			UpdateOption(OPT_PCH, "stdafx.h");
-		else if (ttFileExists("pch.h"))
-			UpdateOption(OPT_PCH, "pch.h");
-		else if (ttFileExists("precomp.h"))
-			UpdateOption(OPT_PCH, "precomp.h");
+    if (ttIsEmpty(GetPchHeader())) {
+        if (ttFileExists("stdafx.h"))
+            UpdateOption(OPT_PCH, "stdafx.h");
+        else if (ttFileExists("pch.h"))
+            UpdateOption(OPT_PCH, "pch.h");
+        else if (ttFileExists("precomp.h"))
+            UpdateOption(OPT_PCH, "precomp.h");
 
-		else if (ttFileExists("pch.hh"))
-			UpdateOption(OPT_PCH, "pch.hh");
-		else if (ttFileExists("pch.hpp"))
-			UpdateOption(OPT_PCH, "pch.hpp");
-		else if (ttFileExists("pch.hxx"))
-			UpdateOption(OPT_PCH, "pch.hxx");
-	}
+        else if (ttFileExists("pch.hh"))
+            UpdateOption(OPT_PCH, "pch.hh");
+        else if (ttFileExists("pch.hpp"))
+            UpdateOption(OPT_PCH, "pch.hpp");
+        else if (ttFileExists("pch.hxx"))
+            UpdateOption(OPT_PCH, "pch.hxx");
+    }
 }
 
 void CTabOptions::OnBegin(void)
 {
-	EnableShadeBtns();
-	SetBtnIcon(DLG_ID(IDOK), IDICON_TTLIB_OK);
-	SetBtnIcon(DLG_ID(IDCANCEL), IDICON_TTLIB_CANCEL);
+    EnableShadeBtns();
+    SetBtnIcon(DLG_ID(IDOK), IDICON_TTLIB_OK);
+    SetBtnIcon(DLG_ID(IDCANCEL), IDICON_TTLIB_CANCEL);
 
-	TC_ITEM ti;
-	ZeroMemory(&ti, sizeof(TC_ITEM));
-	ti.iImage = -1;
+    TC_ITEM ti;
+    ZeroMemory(&ti, sizeof(TC_ITEM));
+    ti.iImage = -1;
 
-	ti.mask = TCIF_TEXT;
-	ti.pszText = (char*) ttGetResString(IDS_GENERAL);
+    ti.mask = TCIF_TEXT;
+    ti.pszText = (char*) ttGetResString(IDS_GENERAL);
 
 #ifdef _DEBUG
-	auto result =
+    auto result =
 #endif
-	SendItemMsg(IDTAB, TCM_INSERTITEM, TAB_GENERAL, (LPARAM) &ti);
-	ttASSERT(result == 0);
+    SendItemMsg(IDTAB, TCM_INSERTITEM, TAB_GENERAL, (LPARAM) &ti);
+    ttASSERT(result == 0);
 
-	ti.pszText = (char*) ttGetResString(IDS_TAB_COMPILER);
-	SendItemMsg(IDTAB, TCM_INSERTITEM, TAB_COMPILER, (LPARAM) &ti);
+    ti.pszText = (char*) ttGetResString(IDS_TAB_COMPILER);
+    SendItemMsg(IDTAB, TCM_INSERTITEM, TAB_COMPILER, (LPARAM) &ti);
 
-	ti.pszText = (char*) ttGetResString(IDS_TAB_CLANG);
-	SendItemMsg(IDTAB, TCM_INSERTITEM, TAB_CLANG, (LPARAM) &ti);
+    ti.pszText = (char*) ttGetResString(IDS_TAB_CLANG);
+    SendItemMsg(IDTAB, TCM_INSERTITEM, TAB_CLANG, (LPARAM) &ti);
 
-	ti.pszText = (char*) ttGetResString(IDS_TAB_LINKER);
-	SendItemMsg(IDTAB, TCM_INSERTITEM, TAB_LINKER, (LPARAM) &ti);
+    ti.pszText = (char*) ttGetResString(IDS_TAB_LINKER);
+    SendItemMsg(IDTAB, TCM_INSERTITEM, TAB_LINKER, (LPARAM) &ti);
 
-	ti.pszText = (char*) ttGetResString(IDS_TAB_RC_MIDL);
-	SendItemMsg(IDTAB, TCM_INSERTITEM, TAB_RC_MIDL, (LPARAM) &ti);
+    ti.pszText = (char*) ttGetResString(IDS_TAB_RC_MIDL);
+    SendItemMsg(IDTAB, TCM_INSERTITEM, TAB_RC_MIDL, (LPARAM) &ti);
 
-	ti.pszText = (char*) ttGetResString(IDS_TAB_SCRIPTS);
-	SendItemMsg(IDTAB, TCM_INSERTITEM, TAB_SCRIPTS, (LPARAM) &ti);
+    ti.pszText = (char*) ttGetResString(IDS_TAB_SCRIPTS);
+    SendItemMsg(IDTAB, TCM_INSERTITEM, TAB_SCRIPTS, (LPARAM) &ti);
 
 #ifdef PRIVATE
-	if (ttFileExists(".private/.srcfiles")) {
-		ti.pszText = (char*) ttGetResString(IDS_TAB_PRIVATE);
-		SendItemMsg(IDTAB, TCM_INSERTITEM, TAB_PRIVATE, (LPARAM) &ti);
-	}
+    if (ttFileExists(".private/.srcfiles"))
+    {
+        ti.pszText = (char*) ttGetResString(IDS_TAB_PRIVATE);
+        SendItemMsg(IDTAB, TCM_INSERTITEM, TAB_PRIVATE, (LPARAM) &ti);
+    }
 #endif
 
-	SendItemMsg(IDTAB, TCM_SETCURSEL, TAB_GENERAL);
-	m_hwndTabSub = m_tabGeneral.DoModeless(*this);
-	::ShowWindow(m_hwndTabSub, SW_SHOW);
+    SendItemMsg(IDTAB, TCM_SETCURSEL, TAB_GENERAL);
+    m_hwndTabSub = m_tabGeneral.DoModeless(*this);
+    ::ShowWindow(m_hwndTabSub, SW_SHOW);
 }
 
 void CTabOptions::OnOK(void)
 {
-	::SendMessage(m_hwndTabSub, WM_COMMAND, IDOK, 0);
+    ::SendMessage(m_hwndTabSub, WM_COMMAND, IDOK, 0);
 
 #ifdef PRIVATE
-	m_tabPrivate.SaveChanges();
+    m_tabPrivate.SaveChanges();
 #endif
 
-	::SendMessage(m_hwndTabSub, WM_CLOSE, 0, 0);
-	DestroyWindow(*this);
-	PostQuitMessage(IDOK);
+    ::SendMessage(m_hwndTabSub, WM_CLOSE, 0, 0);
+    DestroyWindow(*this);
+    PostQuitMessage(IDOK);
 }
 
 void CTabOptions::OnCancel(void)
 {
-	::SendMessage(m_hwndTabSub, WM_CLOSE, 0, 0);
-	DestroyWindow(*this);
-	PostQuitMessage(IDCANCEL);
+    ::SendMessage(m_hwndTabSub, WM_CLOSE, 0, 0);
+    DestroyWindow(*this);
+    PostQuitMessage(IDCANCEL);
 }
 
 void CTabOptions::SaveChanges()
 {
-	if (ttFileExists(txtSrcFilesFileName)) {
-		if (WriteUpdates())
-			puts(".srcfiles Options: section updated");
-	}
-	else {
-		if (WriteUpdates())
-			puts(".srcfiles created");
-	}
+    if (ttFileExists(txtSrcFilesFileName))
+    {
+        if (WriteUpdates())
+            puts(".srcfiles Options: section updated");
+    }
+    else {
+        if (WriteUpdates())
+            puts(".srcfiles created");
+    }
 }
 
 LRESULT CTabOptions::OnNotify(int /* id */, NMHDR* pNmHdr)
 {
-	switch (pNmHdr->code) {
-		case TCN_SELCHANGING:
-			::SendMessage(m_hwndTabSub, WM_COMMAND, IDOK, 0);
-			break;
+    switch (pNmHdr->code)
+    {
+        case TCN_SELCHANGING:
+            ::SendMessage(m_hwndTabSub, WM_COMMAND, IDOK, 0);
+            break;
 
-		case TCN_SELCHANGE:
-			{
-				auto curTab = SendItemMsg(IDTAB, TCM_GETCURSEL);
-				switch (curTab) {
-					case TAB_GENERAL:
-						m_hwndTabSub = m_tabGeneral.DoModeless(*this);
-						break;
+        case TCN_SELCHANGE:
+            {
+                auto curTab = SendItemMsg(IDTAB, TCM_GETCURSEL);
+                switch (curTab) {
+                    case TAB_GENERAL:
+                        m_hwndTabSub = m_tabGeneral.DoModeless(*this);
+                        break;
 
-					case TAB_COMPILER:
-						m_hwndTabSub = m_tabCompiler.DoModeless(*this);
-						break;
+                    case TAB_COMPILER:
+                        m_hwndTabSub = m_tabCompiler.DoModeless(*this);
+                        break;
 
-					case TAB_CLANG:
-						m_hwndTabSub = m_tabCLang.DoModeless(*this);
-						break;
+                    case TAB_CLANG:
+                        m_hwndTabSub = m_tabCLang.DoModeless(*this);
+                        break;
 
-					case TAB_LINKER:
-						m_hwndTabSub = m_tabLinker.DoModeless(*this);
-						break;
+                    case TAB_LINKER:
+                        m_hwndTabSub = m_tabLinker.DoModeless(*this);
+                        break;
 
-					case TAB_RC_MIDL:
-						m_hwndTabSub = m_tabRcMidl.DoModeless(*this);
-						break;
+                    case TAB_RC_MIDL:
+                        m_hwndTabSub = m_tabRcMidl.DoModeless(*this);
+                        break;
 
-					case TAB_SCRIPTS:
-						m_hwndTabSub = m_tabScripts.DoModeless(*this);
-						break;
+                    case TAB_SCRIPTS:
+                        m_hwndTabSub = m_tabScripts.DoModeless(*this);
+                        break;
 #ifdef PRIVATE
-					case TAB_PRIVATE:
-						m_hwndTabSub = m_tabPrivate.DoModeless(*this);
-						break;
+                    case TAB_PRIVATE:
+                        m_hwndTabSub = m_tabPrivate.DoModeless(*this);
+                        break;
 #endif
 
-					default:
-						return 0;
-				}
-				::ShowWindow(m_hwndTabSub, SW_SHOW);
-			}
-			break;
+                    default:
+                        return 0;
+                }
+                ::ShowWindow(m_hwndTabSub, SW_SHOW);
+            }
+            break;
 
-		default:
-			break;
-	}
+        default:
+            break;
+    }
 
-	return 0;
+    return 0;
 }
--- src/tabcompiler.cpp
+++ src/tabcompiler.cpp
@@ -1,168 +1,175 @@
 /////////////////////////////////////////////////////////////////////////////
-// Name:		CTabCompiler
-// Purpose:		IDTAB_COMPILER dialog handler
-// Author:		Ralph Walden
-// Copyright:	Copyright (c) 2019 KeyWorks Software (Ralph Walden)
-// License:		Apache License (see ../LICENSE)
+// Name:      CTabCompiler
+// Purpose:   IDTAB_COMPILER dialog handler
+// Author:    Ralph Walden
+// Copyright: Copyright (c) 2019 KeyWorks Software (Ralph Walden)
+// License:   Apache License (see ../LICENSE)
 /////////////////////////////////////////////////////////////////////////////
 
 #include "pch.h"
 
-#include <ttfiledlg.h>	// ttCFileDlg
+#include <ttfiledlg.h>  // ttCFileDlg
 
 #include "dlgoptions.h"
 
 void CTabCompiler::OnBegin(void)
 {
-	EnableShadeBtns();
-	SetCheck(DLG_ID(m_pOpts->IsOptimizeSpeed() ? IDC_RADIO_SPEED : IDC_RADIO_SPACE));
-	SetCheck(DLG_ID(m_pOpts->GetBoolOption(OPT_STDCALL) ? IDC_RADIO_STDCALL :IDC_RADIO_CDECL));
+    EnableShadeBtns();
+    SetCheck(DLG_ID(m_pOpts->IsOptimizeSpeed() ? IDC_RADIO_SPEED : IDC_RADIO_SPACE));
+    SetCheck(DLG_ID(m_pOpts->GetBoolOption(OPT_STDCALL) ? IDC_RADIO_STDCALL :IDC_RADIO_CDECL));
 
-	ptrdiff_t warnLevel = m_pOpts->GetOption(OPT_WARN_LEVEL) ? ttAtoi(m_pOpts->GetOption(OPT_WARN_LEVEL)) : 4;
+    ptrdiff_t warnLevel = m_pOpts->GetOption(OPT_WARN_LEVEL) ? ttAtoi(m_pOpts->GetOption(OPT_WARN_LEVEL)) : 4;
 
-	switch (warnLevel) {
-		case 1:
-			SetCheck(DLG_ID(IDRADIO_WARN1));
-			break;
-		case 2:
-			SetCheck(DLG_ID(IDRADIO_WARN2));
-			break;
-		case 3:
-			SetCheck(DLG_ID(IDRADIO_WARN3));
-			break;
-		case 4:
-		default:
-			SetCheck(DLG_ID(IDRADIO_WARN4));
-			break;
-	}
+    switch (warnLevel)
+    {
+        case 1:
+            SetCheck(DLG_ID(IDRADIO_WARN1));
+            break;
+        case 2:
+            SetCheck(DLG_ID(IDRADIO_WARN2));
+            break;
+        case 3:
+            SetCheck(DLG_ID(IDRADIO_WARN3));
+            break;
+        case 4:
+        default:
+            SetCheck(DLG_ID(IDRADIO_WARN4));
+            break;
+    }
 
-	if (m_pOpts->GetPchHeader())
-		SetControlText(DLG_ID(IDEDIT_PCH), m_pOpts->GetPchHeader());
-	if (m_pOpts->GetPchCpp())
-		SetControlText(DLG_ID(IDEDIT_PCH_CPP), m_pOpts->GetPchCpp());
-	if (m_pOpts->GetOption(OPT_INC_DIRS))
-		SetControlText(DLG_ID(IDEDIT_INCDIRS), m_pOpts->GetOption(OPT_INC_DIRS));
+    if (m_pOpts->GetPchHeader())
+        SetControlText(DLG_ID(IDEDIT_PCH), m_pOpts->GetPchHeader());
+    if (m_pOpts->GetPchCpp())
+        SetControlText(DLG_ID(IDEDIT_PCH_CPP), m_pOpts->GetPchCpp());
+    if (m_pOpts->GetOption(OPT_INC_DIRS))
+        SetControlText(DLG_ID(IDEDIT_INCDIRS), m_pOpts->GetOption(OPT_INC_DIRS));
 
-	if (m_pOpts->GetOption(OPT_CFLAGS_CMN))
-		SetControlText(DLG_ID(IDEDIT_COMMON), m_pOpts->GetOption(OPT_CFLAGS_CMN));
-	if (m_pOpts->GetOption(OPT_CFLAGS_REL))
-		SetControlText(DLG_ID(IDEDIT_RELEASE), m_pOpts->GetOption(OPT_CFLAGS_REL));
-	if (m_pOpts->GetOption(OPT_CFLAGS_DBG))
-		SetControlText(DLG_ID(IDEDIT_DEBUG), m_pOpts->GetOption(OPT_CFLAGS_DBG));
+    if (m_pOpts->GetOption(OPT_CFLAGS_CMN))
+        SetControlText(DLG_ID(IDEDIT_COMMON), m_pOpts->GetOption(OPT_CFLAGS_CMN));
+    if (m_pOpts->GetOption(OPT_CFLAGS_REL))
+        SetControlText(DLG_ID(IDEDIT_RELEASE), m_pOpts->GetOption(OPT_CFLAGS_REL));
+    if (m_pOpts->GetOption(OPT_CFLAGS_DBG))
+        SetControlText(DLG_ID(IDEDIT_DEBUG), m_pOpts->GetOption(OPT_CFLAGS_DBG));
 
-	SetCheck(DLG_ID(IDC_CHECK_PERMISSIVE), m_pOpts->GetBoolOption(OPT_PERMISSIVE));
+    SetCheck(DLG_ID(IDC_CHECK_PERMISSIVE), m_pOpts->GetBoolOption(OPT_PERMISSIVE));
 }
 
 void CTabCompiler::OnOK(void)
 {
-	ttCStr csz;
+    ttCStr csz;
 
-	csz.GetWindowText(GetDlgItem(DLG_ID(IDEDIT_COMMON)));
-	m_pOpts->UpdateOption(OPT_CFLAGS_CMN, (char*) csz);
+    csz.GetWindowText(GetDlgItem(DLG_ID(IDEDIT_COMMON)));
+    m_pOpts->UpdateOption(OPT_CFLAGS_CMN, (char*) csz);
 
-	csz.GetWindowText(GetDlgItem(DLG_ID(IDEDIT_RELEASE)));
-	m_pOpts->UpdateOption(OPT_CFLAGS_REL, (char*) csz);
+    csz.GetWindowText(GetDlgItem(DLG_ID(IDEDIT_RELEASE)));
+    m_pOpts->UpdateOption(OPT_CFLAGS_REL, (char*) csz);
 
-	csz.GetWindowText(GetDlgItem(DLG_ID(IDEDIT_DEBUG)));
-	m_pOpts->UpdateOption(OPT_CFLAGS_DBG, (char*) csz);
+    csz.GetWindowText(GetDlgItem(DLG_ID(IDEDIT_DEBUG)));
+    m_pOpts->UpdateOption(OPT_CFLAGS_DBG, (char*) csz);
 
-	csz.GetWindowText(GetDlgItem(DLG_ID(IDEDIT_INCDIRS)));
-	m_pOpts->UpdateOption(OPT_INC_DIRS, (char*) csz);
+    csz.GetWindowText(GetDlgItem(DLG_ID(IDEDIT_INCDIRS)));
+    m_pOpts->UpdateOption(OPT_INC_DIRS, (char*) csz);
 
-	csz.GetWindowText(GetDlgItem(DLG_ID(IDEDIT_PCH)));
-	if (csz.IsEmpty())
-		m_pOpts->UpdateOption(OPT_PCH, "none");
-	else
-		m_pOpts->UpdateOption(OPT_PCH, (char*) csz);
+    csz.GetWindowText(GetDlgItem(DLG_ID(IDEDIT_PCH)));
+    if (csz.IsEmpty())
+        m_pOpts->UpdateOption(OPT_PCH, "none");
+    else
+        m_pOpts->UpdateOption(OPT_PCH, (char*) csz);
 
-	// We let WriteSrcFiles handle the case where the base names of PCH and PCH_CPP are identical
+    // We let WriteSrcFiles handle the case where the base names of PCH and PCH_CPP are identical
 
-	csz.GetWindowText(GetDlgItem(DLG_ID(IDEDIT_PCH_CPP)));
-	if (csz.IsEmpty())
-		m_pOpts->UpdateOption(OPT_PCH_CPP, "none");
-	else
-		m_pOpts->UpdateOption(OPT_PCH_CPP, (char*) csz);
+    csz.GetWindowText(GetDlgItem(DLG_ID(IDEDIT_PCH_CPP)));
+    if (csz.IsEmpty())
+        m_pOpts->UpdateOption(OPT_PCH_CPP, "none");
+    else
+        m_pOpts->UpdateOption(OPT_PCH_CPP, (char*) csz);
 
-	if (GetCheck(DLG_ID(IDC_RADIO_SPEED)))
-		m_pOpts->UpdateOption(OPT_OPTIMIZE, "speed");
-	else
-		m_pOpts->UpdateOption(OPT_OPTIMIZE, "space");
+    if (GetCheck(DLG_ID(IDC_RADIO_SPEED)))
+        m_pOpts->UpdateOption(OPT_OPTIMIZE, "speed");
+    else
+        m_pOpts->UpdateOption(OPT_OPTIMIZE, "space");
 
-	if (GetCheck(DLG_ID(IDRADIO_WARN1)))
-		m_pOpts->UpdateOption(OPT_WARN_LEVEL, "1");
-	else if (GetCheck(DLG_ID(IDRADIO_WARN2)))
-		m_pOpts->UpdateOption(OPT_WARN_LEVEL, "2");
-	else if (GetCheck(DLG_ID(IDRADIO_WARN3)))
-		m_pOpts->UpdateOption(OPT_WARN_LEVEL, "3");
-	else if (GetCheck(DLG_ID(IDRADIO_WARN4)))
-		m_pOpts->UpdateOption(OPT_WARN_LEVEL, "4");
+    if (GetCheck(DLG_ID(IDRADIO_WARN1)))
+        m_pOpts->UpdateOption(OPT_WARN_LEVEL, "1");
+    else if (GetCheck(DLG_ID(IDRADIO_WARN2)))
+        m_pOpts->UpdateOption(OPT_WARN_LEVEL, "2");
+    else if (GetCheck(DLG_ID(IDRADIO_WARN3)))
+        m_pOpts->UpdateOption(OPT_WARN_LEVEL, "3");
+    else if (GetCheck(DLG_ID(IDRADIO_WARN4)))
+        m_pOpts->UpdateOption(OPT_WARN_LEVEL, "4");
 
-	m_pOpts->UpdateOption(OPT_PERMISSIVE, GetCheck(DLG_ID(IDC_CHECK_PERMISSIVE)));
-	m_pOpts->UpdateOption(OPT_STDCALL, GetCheck(DLG_ID(IDC_RADIO_STDCALL)));
+    m_pOpts->UpdateOption(OPT_PERMISSIVE, GetCheck(DLG_ID(IDC_CHECK_PERMISSIVE)));
+    m_pOpts->UpdateOption(OPT_STDCALL, GetCheck(DLG_ID(IDC_RADIO_STDCALL)));
 }
 
 void CTabCompiler::OnBtnChangePch()
 {
-	ttCFileDlg fdlg(*this);
-	fdlg.SetFilter("Header Files|*.h;*.hh;*.hpp;*.hxx||");
-	ttCStr cszCWD;
-	cszCWD.GetCWD();
-	fdlg.UseCurrentDirectory();
-	fdlg.RestoreDirectory();
-	if (fdlg.GetOpenFileName()) {
-		ttCStr cszOrg, cszCpp;
-		cszOrg.GetWindowText(GetDlgItem(DLG_ID(IDEDIT_PCH)));
-		cszCpp.GetWindowText(GetDlgItem(DLG_ID(IDEDIT_PCH_CPP)));
+    ttCFileDlg fdlg(*this);
+    fdlg.SetFilter("Header Files|*.h;*.hh;*.hpp;*.hxx||");
+    ttCStr cszCWD;
+    cszCWD.GetCWD();
+    fdlg.UseCurrentDirectory();
+    fdlg.RestoreDirectory();
+    if (fdlg.GetOpenFileName())
+    {
+        ttCStr cszOrg, cszCpp;
+        cszOrg.GetWindowText(GetDlgItem(DLG_ID(IDEDIT_PCH)));
+        cszCpp.GetWindowText(GetDlgItem(DLG_ID(IDEDIT_PCH_CPP)));
 
-		ttCStr cszRelPath;
-		ttConvertToRelative(cszCWD, fdlg.GetFileName(), cszRelPath);
-		SetControlText(DLG_ID(IDEDIT_PCH), cszRelPath);
+        ttCStr cszRelPath;
+        ttConvertToRelative(cszCWD, fdlg.GetFileName(), cszRelPath);
+        SetControlText(DLG_ID(IDEDIT_PCH), cszRelPath);
 
-		if (ttFileExists(cszCpp))	// if the current source file exists, assume that's what the user wants
-			return;
+        if (ttFileExists(cszCpp))   // if the current source file exists, assume that's what the user wants
+            return;
 
-		// The default behaviour is to use the same base name for the C++ source file as the precompiled header file. So, if they are the same, then
-		// change the source file name to match (provided the file actually exists).
+        // The default behaviour is to use the same base name for the C++ source file as the precompiled header file. So, if they are the same, then
+        // change the source file name to match (provided the file actually exists).
 
-		cszOrg.RemoveExtension();
-		cszCpp.RemoveExtension();
+        cszOrg.RemoveExtension();
+        cszCpp.RemoveExtension();
 
-		if (ttIsSameStrI(cszOrg, cszCpp)) {
-			cszCpp = cszRelPath;
-			cszCpp.ChangeExtension(".cpp");
-			if (ttFileExists(cszCpp)) {
-				SetControlText(DLG_ID(IDEDIT_PCH_CPP), cszCpp);
-				return;
-			}
-			cszCpp.ChangeExtension(".cxx");
-			if (ttFileExists(cszCpp)) {
-				SetControlText(DLG_ID(IDEDIT_PCH_CPP), cszCpp);
-				return;
-			}
-			cszCpp.ChangeExtension(".cc");
-			if (ttFileExists(cszCpp)) {
-				SetControlText(DLG_ID(IDEDIT_PCH_CPP), cszCpp);
-				return;
-			}
+        if (ttIsSameStrI(cszOrg, cszCpp))
+        {
+            cszCpp = cszRelPath;
+            cszCpp.ChangeExtension(".cpp");
+            if (ttFileExists(cszCpp))
+            {
+                SetControlText(DLG_ID(IDEDIT_PCH_CPP), cszCpp);
+                return;
+            }
+            cszCpp.ChangeExtension(".cxx");
+            if (ttFileExists(cszCpp))
+            {
+                SetControlText(DLG_ID(IDEDIT_PCH_CPP), cszCpp);
+                return;
+            }
+            cszCpp.ChangeExtension(".cc");
+            if (ttFileExists(cszCpp))
+            {
+                SetControlText(DLG_ID(IDEDIT_PCH_CPP), cszCpp);
+                return;
+            }
 
-			cszCpp.Delete();
-			cszCpp.printf("%s does not have a matching C++ source file -- precompiled header will fail without it!", (char*) cszRelPath);
-			ttMsgBox(cszCpp);
-		}
-	}
+            cszCpp.Delete();
+            cszCpp.printf("%s does not have a matching C++ source file -- precompiled header will fail without it!", (char*) cszRelPath);
+            ttMsgBox(cszCpp);
+        }
+    }
 }
 
 void CTabCompiler::OnBtnPchCpp()
 {
-	ttCFileDlg fdlg(*this);
-	fdlg.SetFilter("C++ Files|*.cpp;*.cc;*.cxx||");
-	fdlg.UseCurrentDirectory();
-	fdlg.RestoreDirectory();
-	if (fdlg.GetOpenFileName()) {
-		ttCStr cszCWD;
-		cszCWD.GetCWD();
-		ttCStr cszRelPath;
-		ttConvertToRelative(cszCWD, fdlg.GetFileName(), cszRelPath);
-		SetControlText(DLG_ID(IDEDIT_PCH_CPP), cszRelPath);
-	}
+    ttCFileDlg fdlg(*this);
+    fdlg.SetFilter("C++ Files|*.cpp;*.cc;*.cxx||");
+    fdlg.UseCurrentDirectory();
+    fdlg.RestoreDirectory();
+    if (fdlg.GetOpenFileName())
+    {
+        ttCStr cszCWD;
+        cszCWD.GetCWD();
+        ttCStr cszRelPath;
+        ttConvertToRelative(cszCWD, fdlg.GetFileName(), cszRelPath);
+        SetControlText(DLG_ID(IDEDIT_PCH_CPP), cszRelPath);
+    }
 }
