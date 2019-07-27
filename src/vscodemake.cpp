dc9953d03af1b8a12bdd37b389662ca0cb2cf5b7
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2019-07-27 07:49:45 -0700

    Skeleton of code to create a custom makefile in .vscode/ directory

---
+++ src/vscodemake.cpp
@@ -0,0 +1,31 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:      vscodemake.cpp
+// Purpose:   Creates .vscode/makefile
+// Author:    Ralph Walden
+// Copyright: Copyright (c) 2019 KeyWorks Software (Ralph Walden)
+// License:   Apache License (see ../LICENSE)
+/////////////////////////////////////////////////////////////////////////////
+
+/*
+    CNinja::CreateMakeFile is used to create a general purpose makefile without knowledge of the user's system.
+
+    CNinja::VsCodeMakefile creates a makefile that is highly customized to the user's machine at the time the makefile
+    was created. It will differ based on the platform, the compiler available to it, and it will assume MakeNinja.exe is available
+    since that was what was used to create it.
+
+*/
+
+
+#include "pch.h"
+
+#include "ninja.h"    // CNinja
+
+// Used to create a makefile in .vscode
+
+bool CNinja::VsCodeMakefile()
+{
+    if (ttFileExists(".vsCode/makefile"))
+        return true;    // TODO: [KeyWorks - 7/26/2019] If we end up storing the MSVC path in the makefile, then we need to be able to update it.
+
+    return false;
+}
