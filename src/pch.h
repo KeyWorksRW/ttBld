c2f13f6a061dd75a024a94c1164adac10f422fc0
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2019-11-11 06:21:17 -0800

    Switches to ttLibwx, and now requires wxWidgets to build.

--- src/pch.h
+++ src/pch.h
@@ -2,61 +2,41 @@
 
 #pragma once
 
-#if defined(_WIN32) && !defined(wxUSE_GUI)
-    #define NOATOM
-    #define NOCOMM
-    #define NODRIVERS
-    #define NOEXTDEVMODEPROPSHEET
-    #define NOIME
-    #define NOKANJI
-    #define NOLOGERROR
-    #define NOMCX
-    #define NOPROFILER
-    #define NOSCALABLEFONT
-    #define NOSERVICE
-    #define NOSOUND
-    #define NOWINDOWSX
-    #define NOENHMETAFILE
+#if defined(_WIN32)
+    #define wxMSVC_VERSION_AUTO
+#endif
 
-    #define OEMRESOURCE
-    #define WIN32_LEAN_AND_MEAN
-    #define _CRT_SECURE_NO_WARNINGS
+#define wxUSE_UNICODE     1
+#define wxUSE_NO_MANIFEST 1  // this is required for clang-cl to work
+#define wxUSE_INTL        1
 
-    #define WINVER       0x0601  // Windows 7
-    #define _WIN32_WINNT 0x0600
-    #define _WIN32_IE    0x0700
+#include "wx/defs.h"
+#include "wx/chartype.h"
 
-    #include <windows.h>
-    #include <stdint.h>
-    #include <stdio.h>
-    #include <stdlib.h>
-#endif    // defined(_WIN32) && !defined(wxUSE_GUI)
+#if defined(__WINDOWS__)
+    #include "wx/msw/wrapwin.h"
+    #include "wx/msw/private.h"
+#endif
 
-#if defined(wxUSE_GUI)
-    #define wxUSE_UNICODE     1
-    #define wxUSE_NO_MANIFEST 1  // This is the only way clang-cl will be able to compile
+#if defined(__WXMSW__)
+    #include "wx/msw/wrapcctl.h"
+    #include "wx/msw/wrapcdlg.h"
+    #include "wx/msw/missing.h"
+#endif
 
-    #include "wx/defs.h"
-    #include "wx/chartype.h"
+#include <wx/translation.h>
 
-    #if defined(__WINDOWS__)
-        #include "wx/msw/wrapwin.h"
-        #include "wx/msw/private.h"
-    #endif
-
-    #if defined(__WXMSW__)
-        #include "wx/msw/wrapcctl.h"
-        #include "wx/msw/wrapcdlg.h"
-        #include "wx/msw/missing.h"
-    #endif
-#endif    // defined(wxUSE_GUI)
-
-extern const char* txtVersion;
-extern const char* txtCopyRight;
+#include <stdexcept>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include <ttlib.h>    // Master header file for ttLib
 #include <ttdebug.h>  // ttASSERT macros
 
 using namespace ttch;  // For the CH_ and CHW_ constants
+
+extern const char* txtVersion;
+extern const char* txtCopyRight;
 
 #include "NinjaStringIds.h"  // Maps IDS_NINJA_ strings into string to translate
